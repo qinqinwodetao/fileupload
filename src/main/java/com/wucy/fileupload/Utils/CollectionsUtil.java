b10bbe3598714750c471af33a606bf98d9969ed1
wuchengyang <wucy1991@163.com>
2017-09-05 22:13:39 +0800

    jdk study

---
+++ src/main/java/com/wucy/fileupload/Utils/CollectionsUtil.java
@@ -0,0 +1,49 @@
+package com.wucy.fileupload.Utils;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class CollectionsUtil {
+
+    private final static int LIST_SIZE = 10000;
+
+    public static void main(String[] args)
+    {
+        List<Integer> arrayList = new ArrayList<>();
+        List<Integer> linkedList = new LinkedList<>();
+
+        for (int i = 0; i < LIST_SIZE; i++)
+        {
+            arrayList.add(i);
+            linkedList.add(i);
+        }
+
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < arrayList.size(); i++)
+        {
+            arrayList.get(i);
+        }
+        System.out.println("ArrayList遍历速度：" + (System.currentTimeMillis() - startTime) + "ms");
+
+        startTime = System.currentTimeMillis();
+        for (int i = 0; i < linkedList.size(); i++)
+        {
+            linkedList.get(i);
+        }
+        System.out.println("普通LinkedList遍历速度：" + (System.currentTimeMillis() - startTime) + "ms");
+
+        startTime = System.currentTimeMillis();
+        for (Integer i : linkedList)
+        {
+            i.toString();
+        }
+
+        System.out.println("增强性for 循环及 Iterator LinkedList遍历速度：" + (System.currentTimeMillis() - startTime) + "ms");
+
+
+    }
+
+
+
+}
