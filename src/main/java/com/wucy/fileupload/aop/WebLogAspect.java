d93c994fb1b41fee9624a721b679dbe536163cc2
wucy <wucy1991@163.com>
2017-11-28 10:15:44 +0800

    add Swagger2

---
+++ src/main/java/com/wucy/fileupload/aop/WebLogAspect.java
@@ -0,0 +1,8 @@
+package com.wucy.fileupload.aop;
+
+/**
+ * @author wucy
+ * Date: 2017-11-27 17:15:00
+ */
+public class WebLogAspect {
+}
---
+++ src/main/java/com/wucy/fileupload/config/Swagger.java
@@ -0,0 +1,8 @@
+package com.wucy.fileupload.config;
+
+/**
+ * @author wucy
+ * Date: 2017-11-27 15:09:00
+ */
+public class Swagger {
+}
